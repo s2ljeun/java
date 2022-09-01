764615cde4c59b2ee509beb8c1ad8b1735cd6925
s2ljeun <s2ljeun@gmail.com>
2022-09-01 17:40:43 +0900

    0901 2

--- day05/src/Exam_08.java
+++ day05/src/Exam_08.java
@@ -1,32 +1,87 @@
-import java.util.Scanner;
-
-/*
- * select sort :: 앞의 수부터 순차적으로 비교해서 위치를 바꿔 정렬
- */
+import java.util.*;
 
 public class Exam_08 {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		int num[] = new int[6];
 		
-		for(int i=0; i<num.length; ++i) {
-			System.out.print(i+1 +"번째 수를 입력해주세요.");
-			num[i] = in.nextInt();
+		int[] arr = new int[10];
+		for(int i=0; i<arr.length; ++i) {
+			System.out.print(i+1+"번째 수를 입력 : ");
+			arr[i] = in.nextInt();
 		}
 		
-		for(int i=0; i<num.length; ++i) {
-			for(int j=i+1; j<num.length; ++j) {
-				if(num[i]>num[j]) {
-					int imsi;
-					imsi = num[i];
-					num[i] = num[j];
-					num[j] = imsi;
+		Arrays.sort(arr);
+
+		/*
+		//insert sort
+		for(int i=1; i<arr.length; ++i) {
+			int temp = arr[i];
+			int j = i-1;
+			while(j>=0) {
+				if (temp < arr[j]) {
+					arr[j+1] = arr[j];
+					j--;
+				}else {
+					arr[j+1] = temp;
+					break;
+				}
+			}
+			if (j<0) {
+				arr[0] = temp;
+			}
+			//temp < arr[i-1],	temp < arr[i-2] ... temp < arr[0]
+			/*
+			for(int j=i-1; j>=0; --j) {
+				if (temp < arr[j]) {
+					arr[j+1] = arr[j];
+				}else {
+					arr[j+1] = temp;
+					break;
+				}
+				if (j==0) {
+					arr[0] = temp;
+				}
+			}*/
+		//}
+		//bubble sort
+		/*
+		for(int i=arr.length-2; i>=0; --i) {
+			for(int j=0; j<=i; ++j) {
+				if (arr[j] > arr[j+1]) {
+					int imsi = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = imsi;
 				}
 			}
 		}
 		
-		for(int i=0; i<num.length; ++i) {
-			System.out.print(num[i]);
+		//select sort
+		/*
+		for(int i=0; i<arr.length-1; ++i) {
+			for(int j=i+1; j<arr.length; ++j) {
+				if (arr[i] > arr[j]) {
+					int imsi = arr[i];
+					arr[i] = arr[j];
+					arr[j] = imsi;
+				}
+			}
 		}
+		*/
+				
+		for(int i=0; i<arr.length-1; ++i) {
+			System.out.printf("%d , ", arr[i]);
+		}
+		System.out.println(arr[arr.length-1]);
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
